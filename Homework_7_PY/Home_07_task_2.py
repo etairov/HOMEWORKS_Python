ae1e3ae25a7b16107578ea8c8b96f2fede295662
e.tairov <ev.tairov@yandex.ru>
2023-04-23 17:49:05 +0700

    Homework_PY_7

--- Homework_7_PY/Home_07_task_2.py
+++ Homework_7_PY/Home_07_task_2.py
@@ -24,3 +24,14 @@ def print_operation_table(operation, num_rows=10, num_columns=10):
 
 
 print_operation_table(lambda x, y: x * y)
+
+#-----------------------------------------------
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for i in range(1, num_rows + 1):
+        for j in range(1, num_columns + 1):
+            print(str(operation(i, j)).rjust(4), end="")
+        print(end="\n")
+
+
+print_operation_table(lambda x, y: x * y, 10, 10)
