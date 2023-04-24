b88e26d6470f1ff06591efc05ee0278acc5ed249
e.tairov <ev.tairov@yandex.ru>
2023-04-24 22:31:51 +0700

    Homework_PY_8

--- Homework_8_PY/Home_08_task_1.py
+++ Homework_8_PY/Home_08_task_1.py
@@ -22,24 +22,53 @@
 # Дополнить телефонный справочник возможностью изменения и удаления данных.
 # Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и удаления данных/
 #------------------------------------------
-
-import csv
-
-for elem in iter(input, '6'):
+def show_data():
     with open('phonebook.csv', 'r', encoding='utf-8') as file:
-        reader = csv.reader(file)
+        book = file.read()
+    return book
+def find_data():
+    with open('phonebook.csv', 'r', encoding='utf-8') as file:
+        book = file.read().split('\n')
+        temp = input('что ищем?: ')
+        for i in book:
+            if temp in i:
+                print(i)
+def new_data():
+    with open('phonebook.csv', 'a', encoding='utf-8') as file:
+        file.write(input('Введите новую строку: ' + '\n'))
+def delete_person(name):
+    persons = show_data()
+    with open('phonebook.csv', "w", encoding="utf8") as file:
+        for person in persons:
+            if name != person:
+                file.write(person)
+def change_person(new_name, old_name):
+    persons = show_data()
+    with open('phonebook.csv', "w", encoding="utf8") as file:
+        for person in persons:
+            if old_name != person:
+                file.write(person)
+            else:
+                file.write(new_name + "\n")
 
-        def display_all_records():
-            for row in reader:
-                print(row)
 
-        if elem == '1':
-            display_all_records()
-
-def read_csv(filename: str) -> list:
-    data = []
-    fields = ["Фамилия", "Имя", "Телефон", "Описание"]
-    with open(filename, 'r', encoding='utf-8') as fin:
-        for line in fin:
-            record = dict(zip(fields, line.strip().split(',')))
-            data.append(record)
+while True:
+    mode = input('Выберите режим работы справочника' + '\n'
+                 + '0-показ файла, 1-поиск данных, 2-добавление в файл, 3-удаление, 4-замена, 5-выход: ')
+    if mode == '0':
+        print(show_data())
+    elif mode == '1':
+        find_data()
+    elif mode == '2':
+        new_data()
+    elif mode == '3':
+        name = input('кого удаляем?: ')
+        delete_person(name)
+    elif mode == '4':
+        old_name = input('кого хотим переименовать?: ')
+        new_name = input('как хотим его назвать?: ')
+        change_person(new_name, old_name)
+    elif mode == '5':
+        break
+    else:
+        print('No mode')
